9a56438f4f4e5231b437ecf360b08cccaa785892
João Pedro Ricardo De Souza <joaopedroricardosouza2005@gmail.com>
2025-08-23 11:39:03 -0400

    Bot 1.1

--- apendendo.py
+++ apendendo.py
@@ -1,13 +1,82 @@
-import telebot
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import urllib.error
+import json
+import time
 import os
+import telebot
 from dotenv import load_dotenv
 
 load_dotenv()
 
 token = os.getenv("BOT_TOKEN")
-
+grupo_id = os.getenv("GRUPO_ID")
+meu_id = os.getenv("MEU_ID")
 bot = telebot.TeleBot(token)
 
-bot.send_message(-1003083634589, "Bom dia")
+url = "https://www.kabum.com.br/produto/621162/ssd-kingston-nv3-1-tb-m-2-2280-pcie-4-0-x4-nvme-leitura-6000-mb-s-gravacao-4000-mb-s-azul-snv3s-1000g"
 
+def formatar_preco(preco):
+    preco_formatado = float((preco.string).replace("R$", "").replace("\u00a0", "").replace(".", "").replace(",", "."))
+    return preco_formatado
 
+def buscar_produto(url):
+    try:
+        page = urlopen(url)
+    except urllib.error.URLError:
+        return None
+    
+    html = page.read().decode("utf-8", errors="ignore")
+    soup = BeautifulSoup(html, "html.parser")
+
+    caixa = soup.find("div", class_="w-full p-8 rounded-8 border-solid border border-black-400")
+    caixa2 = soup.find("div", class_="col-span-4 tablet:col-span-6 desktop:col-span-3 desktop:mt-16 order-1 desktop:order-2")
+    nome_produto = caixa2.find_next("h1", class_="text-sm desktop:text-xl text-black-800 font-bold desktop:font-bold")
+    if caixa and nome_produto:
+        preco = (caixa.find_next("h4", class_="duration-500"))
+        if preco:
+            preco_float = formatar_preco(preco)
+            return {"nome" : (nome_produto.string), "preco": preco_float}
+        else:
+            return 0
+    else:
+        return 0
+
+def ler_produto_antigo(caminho_arquivo):
+    try:
+        with open(caminho_arquivo, mode="r", encoding="utf-8") as read_file:
+            produto = json.load(read_file)
+            produto["preco"] = float(produto["preco"])
+            return produto
+    except FileNotFoundError:
+        return None
+
+def enviar_messagem_telegram(id_do_chat, messagem):
+     bot.send_message(id_do_chat, messagem)
+
+def salvar_produto_novo(caminho_arquivo, produto):
+    with open(caminho_arquivo, mode="w", encoding="utf-8") as file:
+        json.dump(produto, file, indent=0)
+
+while True:
+
+    produto = buscar_produto(url)
+    if produto is None:
+        enviar_messagem_telegram(meu_id, f"A {url} não existe ou está incorreta\n tentando reconexão em 25 min")
+        time.sleep(1500)
+        continue
+    elif produto == 0:
+        enviar_messagem_telegram(meu_id, f"Não foi possivel encontrar o conteudo da pagina {url}.\n Tentando novamente em 5 min")
+        time.sleep(300)
+        continue
+    
+    produto_ant = ler_produto_antigo("produto.json")
+
+    if produto_ant is None or produto_ant != produto:
+        enviar_messagem_telegram(grupo_id, f"{produto['nome']} está custando R$ {produto['preco']}\n {url}")
+        salvar_produto_novo("produto.json", produto)
+    elif produto_ant["preco"] < produto["preco"]:
+        enviar_messagem_telegram(grupo_id, f"O preço aumentou R$ {produto['preco'] - produto_ant['preco']}")
+    elif produto_ant["preco"] > produto["preco"]:
+        enviar_messagem_telegram(grupo_id, f"O preço diminuiu R$ {produto_ant['preco'] - produto['preco']}")
+    time.sleep(3600)
--- teste.py
+++ teste.py
@@ -13,7 +13,49 @@ token = os.getenv("BOT_TOKEN")
 grupo_id = os.getenv("GRUPO_ID")
 bot = telebot.TeleBot(token)
 
-url = "https://www.kabum.com.br/produto/782142/placa-de-video-msi-rtx-5070-12g-shadow-2x-oc-nvdia-geforce-12gb-gddr7-opengl-4-6-g-sync-g5070-12s2c"
+url = "https://www.kabum.com.br/produto/621162/ssd-kingston-nv3-1-tb-m-2-2280-pcie-4-0-x4-nvme-leitura-6000-mb-s-gravacao-4000-mb-s-azul-snv3s-1000g"
+
+def formatar_preco(preco):
+    preco_formatado = float((preco.string).replace("R$", "").replace("\u00a0", "").replace(".", "").replace(",", "."))
+    return preco_formatado
+
+def buscar_produto(url):
+    try:
+        page = urlopen(url)
+    except urllib.error.URLError:
+        return None
+    
+    html = page.read().decode("utf-8", errors="ignore")
+    soup = BeautifulSoup(html, "html.parser")
+
+    caixa = soup.find("div", class_="w-full p-8 rounded-8 border-solid border border-black-400")
+    caixa2 = soup.find("div", class_="col-span-4 tablet:col-span-6 desktop:col-span-3 desktop:mt-16 order-1 desktop:order-2")
+    nome_produto = caixa2.find_next("h1", class_="text-sm desktop:text-xl text-black-800 font-bold desktop:font-bold")
+    if caixa and nome_produto:
+        preco = (caixa.find_next("h4", class_="duration-500"))
+        if preco:
+            preco_float = formatar_preco(preco)
+            return nome_produto, preco_float
+        else:
+            return None
+    else:
+        return None
+
+def ler_preco_antigo(caminho_arquivo):
+    try:
+        with open(caminho_arquivo, mode="r", encoding="utf-8") as read_file:
+            nome_salvo = json.load(read_file)["Nome"]
+            preco_salvo = float(json.load(read_file)["Valor"])
+            return nome_salvo, preco_salvo
+    except FileNotFoundError:
+        return "Arquivo não encontrado"
+
+def enviar_messagem_telegram(messagem):
+     bot.send_message(grupo_id, messagem)
+
+def salvar_preco_novo(caminho_arquivo, produto):
+    with open(caminho_arquivo, mode="w", encoding="utf-8") as file:
+                    json.dump(produto, file, indent=0)
 
 while True:
     try:
@@ -21,7 +63,7 @@ while True:
     except urllib.error.URLError:
         print("URL não existe ou está incorreta")
         print("Tentando reconexão em 25 min")
-        time.sleep(4)
+        time.sleep(1500)
         continue
 
     html = page.read().decode("utf-8", errors="ignore")
@@ -55,4 +97,4 @@ while True:
             bot.send_message(grupo_id, "Não Foi possivel encontrar o preço do produto \n {url}")
     else:
         bot.send_message(grupo_id, "Não foi possivel encontrar o conteudo da pagina")
-    time.sleep(30)
+    time.sleep(3600)
