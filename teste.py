869f39745cff11d0ac86cf2e4bd55f9a1586fe36
João Pedro Ricardo De Souza <joaopedroricardosouza2005@gmail.com>
2025-09-14 13:51:51 -0400

    BD.V0

--- teste.py
+++ teste.py
@@ -1,1 +1,50 @@
 import sqlite3
+
+def iniciar_db():
+    with sqlite3.connect("database.db") as connection:
+        cursor = connection.cursor()
+
+        create_table="""
+        create table if not exists produtos (
+            idp INTEGER PRIMARY KEY AUTOINCREMENT,
+            apelido TEXT,
+            url TEXT NOT NULL,
+            seletor_nome TEXT,
+            seletor_preco TEXT,
+            seletor_disponibilidade TEXT,
+            ultimo_preco REAL,
+            ultima_verificacao TEXT,
+            em_estoque BOOLEAN,
+            fonte varchar(25)
+            );
+            """
+        cursor.execute(create_table)
+
+def listar_produtos():
+    lista_prodrutos = []
+    seleciona = "SELECT * FROM produtos;"
+    with sqlite3.connect("database.db") as connection:
+        cursor = connection.cursor()
+        cursor.execute(seleciona)
+        all_produtos = cursor.fetchall()
+        nome_colunas = [descricao[0] for descricao in cursor.description]
+        if cursor.description:
+            for produtos in all_produtos:
+                produto = {}
+                for i, nome_coluna in enumerate(nome_colunas):
+                    produto[nome_coluna] = produtos[i]
+                lista_prodrutos.append(produto)
+    return lista_prodrutos
+
+a = listar_produtos()
+print(a)
+# create_produto = """
+# insert into produtos (
+#     apelido, url, seletor_nome, seletor_preco, seletor_disponibilidade, ultimo_preco, ultima_verificacao, em_estoque, fonte)
+#     values (?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+
+# info_produto = (('ssd kingston', 'www.kington.com', 'cssnome', 'csspreco', 'cssdisponivel', 1200.50, '2025-08-25', 1, 'Kabum'))
+
+# cursor.execute(create_produto, info_produto)
+
+# connection.commit()
